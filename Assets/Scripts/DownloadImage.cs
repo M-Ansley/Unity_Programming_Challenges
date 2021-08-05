9a71455d49265058af94d0ee2ef47c40a0e771ef
Marcus Ansley <56563445+M-Ansley@users.noreply.github.com>
2021-08-05 21:46:32 +0100

    Unity Web Requests
    Created a script for loading in images using a URL, as well as a script that'll load in a random cat fact from off the web

---
+++ Assets/Scripts/DownloadImage.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class DownloadImage : MonoBehaviour
+{
+    [SerializeField] private RawImage _rawImage;
+    [SerializeField] private string _imageURL;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(GetTexture());
+        }
+    }
+
+    IEnumerator GetTexture()
+    {
+        using (UnityWebRequest request = UnityWebRequestTexture.GetTexture(_imageURL))
+        {
+            yield return request.SendWebRequest();
+            if (request.isHttpError || request.isNetworkError)
+            {
+                Debug.LogError(request.error);
+            }
+            else
+            {
+                Debug.Log("Successfully downloaded image");
+                var texture = DownloadHandlerTexture.GetContent(request);
+                _rawImage.texture = texture;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/DownloadText.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using TMPro;
+
+public class DownloadText : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _text;
+    [SerializeField] private string _textURL;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(GetTexture());
+        }
+    }
+
+    IEnumerator GetTexture()
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(_textURL))
+        {
+            yield return request.SendWebRequest();
+            if (request.isHttpError || request.isNetworkError)
+            {
+                Debug.LogError(request.error);
+            }
+            else
+            {
+                Debug.Log("Successfully downloaded text");
+                
+                var text = request.downloadHandler.text;
+
+                Fact rt = JsonUtility.FromJson<Fact>(text);
+                _text.text = rt.fact;
+            } 
+        }
+    }
+
+    [System.Serializable] // lets us map JSON directly to it
+    public class Fact
+    {
+        public string fact;
+        public int length;
+    }
+
+
+    // CF JSON to C# Class Conversion tool: https://json2csharp.com/
+    // https://catfact.ninja/
+
+}
